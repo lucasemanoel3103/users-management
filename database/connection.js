f72b4a93bf9dceec955c1304122de40ec19b1a6e
Lucas emanoel <lucasemanoel625@gmail.com>
2025-08-18 22:45:51 -0300

    feat: Configuração de ambiente

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const knex = require("knex")({
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }
+});
+module.exports = knex
