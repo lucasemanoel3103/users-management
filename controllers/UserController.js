297db94a1971444336576028b849996ce028dad9
Lucas emanoel <lucasemanoel625@gmail.com>
2025-08-25 20:49:51 -0300

    feat: Cadastro e validação de e-mail

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User")
+
 class UserController{
 
     async index(req, res){
@@ -5,8 +7,24 @@ class UserController{
     }
 
     async create(req, res){
-        console.log(req.body);
-        res.send("Pegando o corpo de requisição");
+      const {email, name, password} = req.body;
+
+      if(email ==undefined){
+        res.status(403).json({err: "O e-mail é inválido!"});
+        return;
+      }
+
+
+      const emailExists = await User.findEmail(email);      
+
+      if(emailExists){
+        res.status(406).json({err: "O e-mail já está cadastrado!"})
+        return;
+      }
+
+      await User.new(email,password,name);
+
+      res.status(201).json("Usuário Criado!");
     }
 };
 
